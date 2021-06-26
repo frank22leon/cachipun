58897aee331e6e42688c9eed5fb694b4a7913267
Francisco leon <22012000fdlq@gmail.com>
2021-06-25 23:44:35 -0400

    firts commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let repetitions = prompt("Ingrese la cantidad de veces que quiere repetir el juego")
+
+const repetitionsQty = Number.parseInt(repetitions)
+
+console.log(`El usuario quiere que el jeugo se repita ${repetitions}`)
+
+for( let count = 1; count <= repetitionsQty; count = count + 1){
+
+    //pedir una respuesta
+}
